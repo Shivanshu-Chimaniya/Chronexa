a73ccb3363c42134fd70278a79906047d2d3eb9b
Shivanshu Chimaniya <Shivanshuchimaniya77@gmail.com>
2024-10-12 17:15:21 +0530

    changed testing code

--- src/pages/AllCapsules.jsx
+++ src/pages/AllCapsules.jsx
@@ -24,7 +24,7 @@ export default function AllCapsules() {
 				allCapsules.push({data, id});
 			});
 
-			setCapsules([]);
+			setCapsules(allCapsules);
 			setIsLoading(false);
 		};
 		getCapsules();
